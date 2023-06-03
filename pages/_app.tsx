c5372cd3ebd602e8de25a95f227bc3c1be3f7ee6
Masanarea <93495976+Masanarea@users.noreply.github.com>
2023-06-03 20:50:59 +0900

    マイプロフィールページを作成(途中)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,19 @@ import '../styles/globals.css'
 
 import 'swiper/swiper-bundle.min.css'
 
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <main className={roboto.className}>
+    <Component {...pageProps} />
+  </main>
+  )
 }
 
 export default MyApp
